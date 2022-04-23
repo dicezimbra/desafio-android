96337e27d84f40ff7ed914879be596bf42823603
Victor <falcaovictor208@gmail.com>
2022-04-22 22:02:50 -0300

    List Item and Pasts created

---
+++ app/src/main/java/com/victorfalcao/myapplication/Utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.victorfalcao.myapplication.Utils
+
+object Constants {
+    const val BASE_URL = "https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1"
+}
---
+++ app/src/main/java/com/victorfalcao/myapplication/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.victorfalcao.myapplication.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.victorfalcao.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
